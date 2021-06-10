8a5e9ef64b3cec8ba80c3c5a1ad7078edaf02e63
QUERY GANI <73988956+QUERY-GANI@users.noreply.github.com>
2021-06-10 18:25:09 +0700

    change edit validation

--- src/middleware/user.validation.ts
+++ src/middleware/user.validation.ts
@@ -11,9 +11,9 @@ const registerValidation = (data: string): Joi.ValidationResult => {
 
 const editValidation = (data: string): Joi.ValidationResult => {
 	const schema = Joi.object({
-		username: Joi.string().min(5).max(20).required(),
-		password: Joi.string().min(7).max(30).required(),
-		email: Joi.string().email({ minDomainSegments: 2 }).required()
+		username: Joi.string().min(5).max(20),
+		password: Joi.string().min(7).max(30),
+		email: Joi.string().email({ minDomainSegments: 2 })
 	});
 
 	return schema.validate(data);
