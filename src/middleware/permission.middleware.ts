ccb630f22844802f3e557bf0fe70cce5cffb16dd
QUERY-GANI <ahmadxgani01@gmail.com>
2021-06-14 11:24:31 +0700

    using cache for performance

--- src/middleware/permission.middleware.ts
+++ src/middleware/permission.middleware.ts
@@ -3,6 +3,13 @@ import { getByEmail } from "../services/users.service";
 import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { CustomRequest } from "../interface/request.interface";
+import NodeCache from "node-cache";
+import { UserDoc } from "interface/mongoose.interface";
+const storage = new NodeCache({
+	stdTTL: 3600,
+	checkperiod: 120,
+	useClones: false
+});
 
 enum roles {
     owner = 1,
@@ -12,10 +19,12 @@ enum roles {
 const auth = (req: CustomRequest, res: Response, next: NextFunction): void|Response<any, Record<string, any>> => {
 	const apiKey = req.headers["x-apikey"];
 	if (!apiKey) return res.status(401).json({ status: res.statusCode, message: "Missing x-apikey in header" });
+	const data: UserDoc = storage.get(apiKey as string) as UserDoc;
 	return jwt.verify(apiKey as string, process.env.SECRET as string, async (e, decoded) => {
 		if (e) return res.status(400).json({ status: res.statusCode, message: "Invalid apikey" });
 		try {
-			const account = await getByEmail((<any>decoded).email);
+			const account = data ? data : await getByEmail((<any>decoded).email);
+			if (!data) storage.set(apiKey as string, account);
 			if ((<any>decoded).role !== account?.role) return res.status(403).json({ status: res.statusCode, message: "Dame desu yo >_<" });
 			req.locals = {
 				account: account,
